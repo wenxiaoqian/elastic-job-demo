83ce64020afd106ebb14aa13089365ae587cf7fc
wenxiaoqian <404190475@qq.com>
2016-10-12 18:23:03 +0800

    update

--- src/main/java/com/welab/job/MyElasticJob.java
+++ src/main/java/com/welab/job/MyElasticJob.java
@@ -1,21 +1,35 @@
 package com.welab.job;
 
-import com.dangdang.ddframe.job.api.AbstractOneOffElasticJob;
-import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
-public class MyElasticJob extends AbstractOneOffElasticJob {
+public class MyElasticJob implements SimpleJob {
 
     private static AtomicInteger count = new AtomicInteger(0);
 
     @Override
-    protected void process(JobExecutionMultipleShardingContext context) {
+    public void execute(ShardingContext shardingContext) {
         // do something by sharding items
-        System.out.println("第"+count.addAndGet(1)+"次执行，当前分片号为："+context.getShardingItemParameters());
-    }
+        System.out.println("第"+count.addAndGet(1)+"次执行，当前分片号为："+shardingContext.getShardingParameter());
 
+        switch (shardingContext.getShardingItem()){
+            case 0:
+                // do something by sharding item 0
+                break;
+            case 1:
+                // do something by sharding item 1
+                break;
+            case 2:
+                // do something by sharding item 2
+                break;
+            default:
+                break;
+        }
+
+    }
 }
